73fdaa010fc9f74cb419d2c0d230c59291251467
Trapo <matoufdanslezoo@gmail.com>
2025-10-06 09:35:29 +0200

    FIX: AUTHCONTROLLER

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -119,7 +119,7 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
         return response()->json(['message' => 'Déconnexion réussie']);
     }
 
